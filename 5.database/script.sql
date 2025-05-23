4ccbc83f95a0c6d4441885e6bc2653c5ce6705e0
santidelmonaco <sdelmonaco@fi.uba.ar>
2025-05-23 16:32:20 -0300

    Actualizar script.sql

--- 5.database/script.sql
+++ 5.database/script.sql
@@ -1,38 +1,51 @@
-CREATE TABLE Usuarios (
-    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE usuario_comercio (
+    id_usr_comercio INT PRIMARY KEY AUTO_INCREMENT,
     nombre_usuario VARCHAR(100),
-    mail VARCHAR(100) UNIQUE,
-    contraseña VARCHAR(100),
-    tipo_de_usuario VARCHAR(50)
+    email_usuario VARCHAR(100) UNIQUE,
+    contrasena VARCHAR(100)
 );
 
-CREATE TABLE Restaurantes (
+CREATE TABLE usuario_final (
+    id_usr INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_usuario VARCHAR(100),
+    email_usuario VARCHAR(100) UNIQUE,
+    contrasena VARCHAR(100)
+);
+
+CREATE TABLE restaurantes (
     id_restaurante INT PRIMARY KEY AUTO_INCREMENT,
-    id_usuario INT,
+    id_usr_comercio INT,
     nombre_restaurante VARCHAR(100),
     categoria VARCHAR(50),
     tipo_de_cocina VARCHAR(100),
     telefono VARCHAR(20),
     ubicacion VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
+    tiempo_de_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    pdf_menu_link TEXT,
+    FOREIGN KEY (id_usr_comercio) REFERENCES usuario_comercio(id_usr_comercio)
 );
 
-CREATE TABLE Platos (
-    id_plato INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE calificaciones (
     id_restaurante INT,
-    nombre_plato VARCHAR(100),
-    info_plato TEXT,
-    FOREIGN KEY (id_restaurante) REFERENCES Restaurantes(id_restaurante)
-);
-
-CREATE TABLE Calificaciones (
-    id_calificacion INT PRIMARY KEY AUTO_INCREMENT,
-    id_usuario INT,
-    id_restaurante INT,
-    puntaje INT CHECK (puntaje BETWEEN 1 AND 5),
+    id_usr INT,
     comentario TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
-    FOREIGN KEY (id_restaurante) REFERENCES Restaurantes(id_restaurante)
+    calificacion INT CHECK (calificacion BETWEEN 1 AND 5),
+    tiempo_de_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_restaurante, id_usr),
+    FOREIGN KEY (id_restaurante) REFERENCES restaurantes(id_restaurante),
+    FOREIGN KEY (id_usr) REFERENCES usuario_final(id_usr)
+);
+
+CREATE TABLE reservas (
+    id_reserva INT PRIMARY KEY AUTO_INCREMENT,
+    id_usr INT,
+    id_restaurante INT,
+    nombre_bajo_reserva VARCHAR(100),
+    email VARCHAR(100),
+    telefono VARCHAR(20),
+    cant_personas INT,
+    fecha_reserva DATETIME,
+    solicitud_especial TEXT,
+    FOREIGN KEY (id_usr) REFERENCES usuario_final(id_usr),
+    FOREIGN KEY (id_restaurante) REFERENCES restaurantes(id_restaurante)
 );
