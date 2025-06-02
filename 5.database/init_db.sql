87293b01d529f9f5ed5efb00f61ade3650c79628
msspza <msapienza@fi.uba.ar>
2025-06-02 11:55:00 -0300

    Agregue a la tabla 'comercios' dos atributos mas y modifique un par de detalles respecto a las FK e los nombres

--- 5.database/init_db.sql
+++ 5.database/init_db.sql
@@ -1,6 +1,6 @@
 -- Crear base de datos
-CREATE DATABASE IF NOT EXISTS sistema_reservas;
-USE sistema_reservas;
+CREATE DATABASE IF NOT EXISTS foodyba_dbb;
+USE foodyba_dbb;
 
 -- Tabla: usuario_comercio
 CREATE TABLE usuario_comercio (
@@ -10,51 +10,52 @@ CREATE TABLE usuario_comercio (
     contrasena VARCHAR(100)
 );
 
--- Tabla: usuario_final
-CREATE TABLE usuario_final (
+-- Tabla: usuario_consumidor
+CREATE TABLE usuario_consumidor (
     id_usr INT PRIMARY KEY AUTO_INCREMENT,
     nombre_usuario VARCHAR(100),
     email_usuario VARCHAR(100) UNIQUE,
     contrasena VARCHAR(100)
 );
 
--- Tabla: restaurantes
-CREATE TABLE restaurantes (
-    id_restaurante INT PRIMARY KEY AUTO_INCREMENT,
+-- Tabla: comercio
+CREATE TABLE comercios (
+    id_comercio INT PRIMARY KEY AUTO_INCREMENT,
     id_usr_comercio INT,
-    nombre_restaurante VARCHAR(100),
+    nombre_comercio VARCHAR(100),
     categoria VARCHAR(50),
     tipo_de_cocina VARCHAR(100),
     telefono VARCHAR(20),
     ubicacion VARCHAR(255),
     tiempo_de_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
     pdf_menu_link TEXT,
+    calificacion FLOAT,                                                 
+    horarios VARCHAR(100)
     FOREIGN KEY (id_usr_comercio) REFERENCES usuario_comercio(id_usr_comercio)
 );
 
--- Tabla: calificaciones
-CREATE TABLE calificaciones (
-    id_restaurante INT,
+-- Tabla: resenias
+CREATE TABLE resenias (
+    id_comercio INT,
     id_usr INT,
     comentario TEXT,
     calificacion INT CHECK (calificacion BETWEEN 1 AND 5),
     tiempo_de_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id_restaurante, id_usr),
-    FOREIGN KEY (id_restaurante) REFERENCES restaurantes(id_restaurante),
-    FOREIGN KEY (id_usr) REFERENCES usuario_final(id_usr)
+    FOREIGN KEY (id_comercio) REFERENCES comercios(id_comercio),
+    FOREIGN KEY (id_usr) REFERENCES usuario_consumidor(id_usr)
 );
 
 -- Tabla: reservas
 CREATE TABLE reservas (
     id_reserva INT PRIMARY KEY AUTO_INCREMENT,
     id_usr INT,
-    id_restaurante INT,
+    id_comercio INT,
     nombre_bajo_reserva VARCHAR(100),
     email VARCHAR(100),
     telefono VARCHAR(20),
     cant_personas INT,
     fecha_reserva DATETIME,
     solicitud_especial TEXT,
-    FOREIGN KEY (id_usr) REFERENCES usuario_final(id_usr),
-    FOREIGN KEY (id_restaurante) REFERENCES restaurantes(id_restaurante)
+    FOREIGN KEY (id_usr) REFERENCES usuario_consumidor(id_usr),
+    FOREIGN KEY (id_comercio) REFERENCES comercios(id_comercio)
 );
