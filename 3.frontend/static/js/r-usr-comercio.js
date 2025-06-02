4ae7390a17bd8251c92a3893e4a8e78be6ce1e7e
msspza <msapienza@fi.uba.ar>
2025-06-01 22:49:59 -0300

    Finalizada parte-2 del registro

--- 3.frontend/static/js/r-usr-comercio.js
+++ 3.frontend/static/js/r-usr-comercio.js
@@ -1,21 +1,22 @@
-const list_input_consumidor=document.getElementsByClassName("usr-consumidor");                              // Todos los input que sean de la clase 'usr-consumidor' van a estar almacenados en 'list_input'. Estos van a ser aquellos utilizados para crear un usuario 'consumidor'
+const block_input_consumidor=document.getElementById("data-usr-consumidor")
+const block_input_comercio=document.getElementById("data-usr-comercio")
 const check_usr_comercio=document.getElementById("check-usr-comercio");
 const check_usr_consumidor=document.getElementById("check-usr-consumidor");
 
 // Si el usuario selecciona la casilla para crear una cuenta del tipo comercio
 check_usr_comercio.addEventListener("change", ()=>{
-    // Aplico una transformación que va a desplazar todos los 'input' de 'usr-consumidor' hacia la izquierda
-    for(let i=0; i<list_input_consumidor.length; i++)
-    {
-        list_input_consumidor[i].style.transform="translateX(-1000px)";
-    }
+    // Aplico una transformación que va a desplazar todo el bloque del usuario comercio hacia la izquierda haciendolos "aparecer" en pantalla
+    block_input_comercio.style.transform="translateX(0px)";
+
+    // Aplico una transformación que va a desplazar todo el bloque del usuario consumidor hacia la izquierda haciendolo "desaparecer" de la pantalla
+    block_input_consumidor.style.transform="translateX(-1000px)";
 });
 
 // Si el usuario selecciona la casilla para crear una cuenta del tipo consumidor
 check_usr_consumidor.addEventListener("change",()=>{
-    // Aplico una transformación que va a desplazar todos los 'input' de 'usr-consumidor' al lugar que estaba previamente
-    for(let i=0; i<list_input_consumidor.length; i++)
-    {
-        list_input_consumidor[i].style.transform="translateX(0px)";
-    }
+    // Aplico una transformación que va a desplazar todo el bloque del usuario consumidor hacia la derecha haciendolo "aparecer" en la pantalla
+    block_input_consumidor.style.transform="translateX(0px)";
+    
+    // Aplico una transformación que va a desplazar todo el bloque del usuario comercio hacia la derecha haciendolo "desaparecer" de la pantalla
+    block_input_comercio.style.transform="translateX(2000px)";
 });
