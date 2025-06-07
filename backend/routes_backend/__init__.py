b2ec43cc148d9bfde314b1f32d1f64d6fc7452e9
Rocco <89814263+roccogb@users.noreply.github.com>
2025-06-07 18:52:00 -0300

    Update __init__.py

--- backend/routes_backend/__init__.py
+++ backend/routes_backend/__init__.py
@@ -1,6 +1,7 @@
 # Modulo que va a establecer a la carpeta 'routes-backend' como un paquete
 from comercios import comercios_bp
 from auth import auth_bp
+from .test_routes import test_bp
 
 # Esta funcion va a registrar cada blueprint en una aplicacion Flask con un prefijo
 def register_routes(app):
