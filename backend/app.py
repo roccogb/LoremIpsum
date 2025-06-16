0d0824559a48d4c606a9f68055dccc0c715b3de5
Rocco <89814263+roccogb@users.noreply.github.com>
2025-06-16 19:07:14 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
-# Modulo que va a ejecutar el back-end de FoodyBA
 from flask import Flask
 from routes_backend import register_routes
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = 'tp-ids-2025'
+app = Flask(__name__,
+    static_folder="../frontend/static",
+    template_folder="../frontend/templates")
 
-register_routes(app)  # Registro los blueprints creados en mi app Flask
+app.config['SECRET_KEY'] = 'tp-ids-2025'
+
+register_routes(app)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8100, debug=True)
+    app.run(debug=True, port=8100)
