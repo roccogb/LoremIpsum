ce6654df7274980cea9e89b5fec6bdc1408d7f04
Lucio99 <lfrete@fi.uba.ar>
2025-06-19 23:27:35 -0300

    Nuevos tests_data.

--- tests/init_test_data.py
+++ tests/init_test_data.py
@@ -6,13 +6,28 @@ cursor = conn.cursor()
 
 ################# REGISTROS DE PRUEBA PARA LA TABLA 'usuario_comercio'
 cursor.execute("""INSERT INTO usuario_comercio 
-               (id_usr_comercio, nombre_apellido, DNI, CUIT, email_usuario, contrasena, fecha_creacion) 
-               VALUES 
-               (59, 'Andrés Iniesta', 30111222, 20301112223, 'ainiesta@gmail.com', 'golazo123', '2025-06-12'),
-               (60, 'Lisa Simpson', 22334455, 27223344556, 'lisamusica@springfield.com', 'saxolove4', '2025-06-13'),
-               (61, 'Pedro Pascal', 35998221, 20359982213, 'mandolorian@hotmail.com', 'babyY0da!', '2025-06-14'),
-               (62, 'Sandra Bullock', 40112333, 23401123334, 'sandra.b@cine.com', 'speed1994', '2025-06-15'),
-               (63, 'Joaquín Sabina', 30011211, 25300112112, 'jsabina@poesia.net', 'y17picon', '2025-06-16');""")
+                 (id_usr_comercio, nombre_apellido, DNI, CUIT, email_usuario, contrasena, fecha_creacion)
+                 VALUES
+                 (64, 'Mariana Fabbiani', 32111222, 27321112221, 'mfabbiani@canal.com', 'tvshow321', '2025-06-16'),
+                 (65, 'Diego Torres', 34123123, 27341231234, 'dtorres@musica.com', 'coloresperanza', '2025-06-16'),
+                 (66, 'Natalie Portman', 32199900, 27321999001, 'n.portman@cine.org', 'blackSwan22', '2025-06-17'),
+                 (67, 'Juan Román Riquelme', 28777222, 20287772223, 'jrr10@futbol.ar', 'enganche10', '2025-06-17'),
+                 (68, 'Emma Watson', 31311311, 27313113112, 'emma.w@hogwarts.uk', 'hermione22', '2025-06-17'),                    
+                 (69, 'Carlos Tévez', 30010101, 20300101012, 'carlitos@boca.ar', 'apache123', '2025-06-18'),
+                 (70, 'Lionel Messi', 31123321, 20311233211, 'lmessi@inter.miami', 'goat10', '2025-06-18'),
+                 (71, 'Frida Kahlo', 29992211, 27299922112, 'frida@arte.mx', 'cejaunic4', '2025-06-18'),
+                 (72, 'Sofía Vergara', 32211333, 27322113334, 'svergara@modern.com', 'gloria2025', '2025-06-19'),
+                 (73, 'Keanu Reeves', 31444444, 27314444445, 'neo@matrix.net', 'whoa123', '2025-06-19'),
+                 (74, 'Gustavo Cerati', 30303030, 27303030301, 'cerati@rock.com', 'mequedo', '2025-06-19'),
+                 (75, 'Amy Winehouse', 31999988, 27319999881, 'amy@jazz.uk', 'back2black', '2025-06-20'),
+                 (76, 'Mafalda Quino', 31122334, 27311223345, 'mafiquino@revista.com', 'nopolitics', '2025-06-20'),
+                 (77, 'Stephen Hawking', 27778888, 27277788889, 'hawking@cosmos.uk', 'time4space', '2025-06-20'),
+                 (78, 'Dua Lipa', 32987654, 27329876541, 'dualipa@pop.com', 'futureNostalgia', '2025-06-20'),
+                 (79, 'Michael Bublé', 32887766, 27328877662, 'mbuble@crooner.ca', 'canada123', '2025-06-20'),
+                 (80, 'Jorge Drexler', 32775511, 27327755113, 'jdrexler@musica.uy', 'guitarra2025', '2025-06-20'),
+                 (81, 'Penélope Cruz', 32445566, 27324455667, 'pcruz@cine.es', 'alma2025', '2025-06-20'),
+                 (82, 'Elon Musk', 30101010, 20301010101, 'elon@tesla.space', 'marsbound', '2025-06-20'),
+                 (83, 'Shakira Mebarak', 30012345, 27300123456, 'shakira@music.co', 'hipsdontlie', '2025-06-20');""")   
 
 ################ REGISTROS DE PRUEBA PARA LA TABLA 'usuario_consumidor'
 cursor.execute("""INSERT INTO usuario_consumidor 
@@ -27,14 +42,28 @@ cursor.execute("""INSERT INTO usuario_consumidor
 
 ################ REGISTROS DE PRUEBA PARA LA TABLA 'comercios'
 cursor.execute("""INSERT INTO comercios
-                  (id_comercio,id_usr_comercio, ruta_imagen,nombre_comercio, categoria, tipo_cocina, telefono, latitud, longitud, tiempo_de_creacion, pdf_menu_link, promedio_calificacion, cantidad_resenias, ranking_ponderado, dias, horarios, etiquetas)
-                  VALUES 
-                  (1, 59, '/resources/uploads/comercios/don_cafe.jpg', 'Don Café', 'especialidad', 'autor', 22113344, -34.6037, -58.3816, '2025-06-15 09:00:00', 'https://doncafe.menu', 4.7, 20, 4.2, "['lunes','martes','miércoles','jueves','viernes']", "['7-11','16-18']", "['wifi','sin_gluten','apto_mascotas']"),
-                  (2, 60, '/resources/uploads/comercios/chimichurri_grill.jpg', 'Chimichurri Grill', 'familiar', 'clasica', 23004567, -34.6100, -58.4000, '2025-06-14 13:15:00', 'https://chimigrill.menu', 0.0, 0, 0.0, "['jueves','viernes','sábado','domingo']", "['12-15','19-23']", "['para_llevar','musica_vivo','zona_fumadores']"),                  
-                  (3, 61, '/resources/uploads/comercios/veggie_vida.jpg', 'Veggie Vida', 'tematico', 'clasica', 22119988, -34.5800, -58.3700, '2025-06-13 10:45:00', 'https://veggievida.menu', 4.9, 5, 4.2, "['lunes','miércoles','viernes']", "['12-15','19-23']", "['vegano','sin_gluten','accesible']"),
-                  (4, 62, '/resources/uploads/comercios/el_gourmet_oculto.jpg', 'El Gourmet Oculto', 'gourmet', 'alta_cocina', 23115678, -34.5933, -58.4100, '2025-06-12 20:00:00', 'https://gourmetoculto.link', 5.0, 1, 5.0, "['viernes','sábado']", "['19-23']", "['happy_hour','musica_vivo','wifi']"),
-                  (5, 63, '/resources/uploads/comercios/pollo_frito_king.jpg', 'Pollo Frito King', 'comida_rapida', 'vanguardia', 29998877, 36.72995945020064, -4.547759190890471, '2025-06-16 11:30:00', 'https://gaskins-fried-chicken.es/#men%C3%BA', 3.9, 80, 3.5, "['lunes','martes','miércoles','jueves','viernes','sábado']", "['12-15','19-23']", "['delivery','para_llevar','wifi']");
-""")
+                    (id_comercio,id_usr_comercio, ruta_imagen,nombre_comercio, categoria, tipo_cocina, telefono, latitud, longitud, tiempo_de_creacion, pdf_menu_link, promedio_calificacion, cantidad_resenias, ranking_ponderado, dias, horarios, etiquetas)
+                    VALUES
+                    (6, 64, '/resources/uploads/comercios/pizza_roma.jpg', 'Pizza Roma', 'familiar', 'alta_cocina', 22334455, -34.6099, -58.3830, '2025-06-16 13:00:00', 'https://doncafe.menu', 4.3, 15, 4.1, "['lunes','martes','miércoles']", "['12-15','19-23']", "['delivery','wifi']"),
+                    (7, 65, '/resources/uploads/comercios/tango_bistro.jpg', 'Tango Bistro', 'tematico', 'tecnico_conceptual', 22334456, -34.6111, -58.3855, '2025-06-16 18:00:00', 'https://doncafe.menu', 4.0, 22, 3.9, "['jueves','viernes','sábado']", "['19-23']", "['musica_vivo','zona_fumadores']"),
+                    (8, 66, '/resources/uploads/comercios/luna_sushi.jpg', 'Luna Sushi', 'gourmet', 'vanguardia', 22334457, -34.5998, -58.3700, '2025-06-17 12:00:00', 'https://doncafe.menu', 4.8, 10, 4.5, "['lunes','miércoles','viernes']", "['12-15']", "['wifi','sin_gluten']"),
+                    (9, 67, '/resources/uploads/comercios/cafe_bombon.jpg', 'Café Bombón', 'especialidad', 'autor', 22334458, -34.6033, -58.3888, '2025-06-17 09:00:00', 'https://doncafe.menu', 4.1, 18, 3.8, "['lunes','martes','jueves']", "['7-11','16-18']", "['apto_mascotas','wifi']"),
+                    (10, 68, '/resources/uploads/comercios/casa_madre.jpg', 'Casa Madre', 'familiar', 'clasica', 22334459, -34.6102, -58.3960, '2025-06-17 20:00:00', 'https://doncafe.menu', 3.7, 8, 3.4, "['miércoles','viernes','domingo']", "['12-15','19-23']", "['para_llevar']"),
+                    (11, 69, '/resources/uploads/comercios/burger_heaven.jpg', 'Burger Heaven', 'comida_rapida', 'alta_cocina', 22334460, -34.6155, -58.4040, '2025-06-18 13:45:00', 'https://doncafe.menu', 4.6, 30, 4.4, "['todos']", "['12-15','19-23']", "['delivery','wifi','zona_fumadores']"),
+                    (12, 70, '/resources/uploads/comercios/parrilla_don_julio.jpg', 'Parrilla Don Julio', 'gourmet', 'tecnico_conceptual', 22334461, -34.6044, -58.3899, '2025-06-18 20:30:00', 'https://doncafe.menu', 5.0, 50, 4.9, "['viernes','sábado','domingo']", "['19-23']", "['musica_vivo','happy_hour']"),
+                    (13, 71, '/resources/uploads/comercios/green_garden.jpg', 'Green Garden', 'tematico', 'vanguardia', 22334462, -34.5876, -58.3777, '2025-06-18 10:15:00', 'https://doncafe.menu', 4.4, 12, 4.0, "['lunes','martes','jueves']", "['12-15']", "['sin_gluten','vegano','apto_mascotas']"),
+                    (14, 72, '/resources/uploads/comercios/arepa_land.jpg', 'Arepa Land', 'familiar', 'autor', 22334463, -34.5991, -58.3755, '2025-06-19 12:00:00', 'https://doncafe.menu', 3.9, 5, 3.6, "['lunes','miércoles','sábado']", "['12-15','19-23']", "['para_llevar']"),
+                    (15, 73, '/resources/uploads/comercios/food_truck_bay.jpg', 'Food Truck Bay', 'comida_rapida', 'clasica', 22334464, -34.6022, -58.3801, '2025-06-19 11:30:00', 'https://doncafe.menu', 4.2, 20, 3.9, "['jueves','viernes','sábado']", "['12-15','19-23']", "['delivery','wifi']"),
+                    (16, 74, '/resources/uploads/comercios/cerveza_artesanal_alegria.jpg', 'Cerveza Alegría', 'tematico', 'alta_cocina', 22334465, -34.6078, -58.4002, '2025-06-19 19:00:00', 'https://doncafe.menu', 4.7, 28, 4.4, "['jueves','viernes','sábado']", "['19-23']", "['happy_hour','musica_vivo']"),
+                    (17, 75, '/resources/uploads/comercios/bistro_luz.jpg', 'Bistró Luz', 'especialidad', 'tecnico_conceptual', 22334466, -34.6090, -58.3950, '2025-06-20 18:45:00', 'https://doncafe.menu', 4.5, 13, 4.3, "['miércoles','jueves','viernes']", "['16-18','19-23']", "['wifi','sin_gluten']"),
+                    (18, 76, '/resources/uploads/comercios/cocina_andina.jpg', 'Cocina Andina', 'tematico', 'vanguardia', 22334467, -34.5900, -58.3733, '2025-06-20 13:30:00', 'https://doncafe.menu', 4.0, 7, 3.7, "['lunes','jueves','sábado']", "['12-15','19-23']", "['delivery','apto_mascotas']"),
+                    (19, 77, '/resources/uploads/comercios/mila_house.jpg', 'Mila House', 'familiar', 'autor', 22334468, -34.6113, -58.3942, '2025-06-20 14:00:00', 'https://doncafe.menu', 4.1, 10, 3.9, "['martes','viernes','domingo']", "['12-15']", "['para_llevar','wifi']"),
+                    (20, 78, '/resources/uploads/comercios/ramen_ya.jpg', 'Ramen Ya!', 'especialidad', 'clasica', 22334469, -34.6080, -58.3911, '2025-06-20 20:15:00', 'https://doncafe.menu', 4.6, 16, 4.2, "['miércoles','sábado']", "['12-15','19-23']", "['wifi','sin_gluten']"),
+                    (21, 79, '/resources/uploads/comercios/empanadas_dona_rosa.jpg', 'Doña Rosa', 'familiar', 'alta_cocina', 22334470, -34.6120, -58.3920, '2025-06-20 19:30:00', 'https://doncafe.menu', 3.8, 6, 3.4, "['martes','jueves','domingo']", "['12-15','19-23']", "['delivery']"),
+                    (22, 80, '/resources/uploads/comercios/helados_polart.jpg', 'Helados Polart', 'especialidad', 'tecnico_conceptual', 22334471, -34.6001, -58.3862, '2025-06-20 16:00:00', 'https://doncafe.menu', 4.2, 25, 4.0, "['lunes','viernes','sábado']", "['16-18','19-23']", "['apto_mascotas','wifi']"),
+                    (23, 81, '/resources/uploads/comercios/tapas_y_vino.jpg', 'Tapas y Vino', 'gourmet', 'vanguardia', 22334472, -34.6055, -58.3933, '2025-06-20 21:00:00', 'https://doncafe.menu', 4.9, 35, 4.8, "['viernes','sábado']", "['19-23']", "['musica_vivo','happy_hour']"),
+                    (24, 82, '/resources/uploads/comercios/bio_natur.jpg', 'Bio Natur', 'tematico', 'autor', 22334473, -34.6032, -58.3977, '2025-06-20 11:00:00', 'https://doncafe.menu', 4.5, 18, 4.1, "['lunes','miércoles','viernes']", "['12-15','19-23']", "['vegano','sin_gluten']"),
+                    (25, 83, '/resources/uploads/comercios/boca_taco.jpg', 'Boca Taco', 'comida_rapida', 'clasica', 22334474, -34.6066, -58.3892, '2025-06-20 13:00:00', 'https://doncafe.menu', 4.0, 12, 3.7, "['jueves','viernes','sábado']", "['12-15','19-23']", "['para_llevar','zona_fumadores']");""")
 
 
 conn.commit()                           # Guardo los cambios realizados en la BDD
